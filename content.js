56376772ea399c07b640c1fb1e54b54c3338c8b7
Alex Derbes <acd@Alexs-MBP.gghc.com>
2016-10-24 11:21:57 -0400

    added foreign suff

--- content.js
+++ content.js
@@ -1,28 +1,41 @@
 
+var exMap = {};
+exMap['.jp'] = 'TYO';
+exMap['.hk'] = 'HKG';
+exMap['.pa'] = 'EPA';
+exMap['.to'] = 'TSE';
+exMap['.ax'] = 'ASX';
+exMap['.as'] = 'EPA';
+exMap['.l'] = 'LON';
+exMap['.br'] = 'EBR';
+exMap['.mi'] = 'BIT';
+
 if ( document.URL.indexOf("security_detail.aspx") != -1)
 {
 	var images = document.getElementsByTagName('img');
-	var re = /http:\/\/chart.yahoo.com\/c\/1y\/.*\/(.*)\.gif/;
+	var re = /http:\/\/chart.yahoo.com\/c\/1y\/.*\/(.*?)(\.[a-z]+)?\.gif/;
 	for (var i = 0, l = images.length; i < l; i++) {
 		var myArray = images[i].src.match(re);
+		console.log(myArray);
 		console.log(images[i].src);
 	
-		if( myArray != null && myArray[1].indexOf(".") == -1 && myArray[1].indexOf(" ") == -1)
+	
+		if(  myArray != null )
 		{
-			// http://stockcharts.com/c-sc/sc?s=NFLX&p=D&yr=2&mn=0&dy=0&i=t62538121274&r=1476818820554
-			images[i].src = 'http://stockcharts.com/c-sc/sc?s=' + myArray[1] + '&p=D&yr=2&mn=0&dy=0&i=t62538121274&r=1476818820554'; //'&p=D&yr=2&mn=0&dy=0&i=t53265630090&r=1476801805335';
+			if( myArray[2] == ".hk" && myArray[1].length == 3 )
+				myArray[1] = "0" + myArray[1];
+			
+			if( myArray[2] == null )
+				images[i].src = 'http://stockcharts.com/c-sc/sc?s=' + myArray[1] + '&p=D&yr=2&mn=0&dy=0&i=t62538121274&r=1476907012474'; 
+			else
+				images[i].src = 'http://www.google.com/finance/getchart?q='+ myArray[1].toUpperCase() +'&x=' + exMap[ myArray[2] ] + '&p=2Y&i=86400';
 		}
-		//console.log(myArray);
 	}
 }
 else 
 {
 		var graph = document.getElementById('Summaries_mmGraph');
-		console.log( graph );
+		console.log("graph" +  graph );
 		paths = graph.getElementsByTagName("path");
-		console.log( paths );
+		console.log(paths );
 }
-//var newURL = 'http://stockcharts.com/c-sc/sc?s=NFLX&p=D&yr=2&mn=0&dy=0&i=t53265630090&r=1476801805335 '
-
-
-// 
